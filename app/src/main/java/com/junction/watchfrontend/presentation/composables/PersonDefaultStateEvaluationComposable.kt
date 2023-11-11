3b00912efc30f16b9a057922cb8db34ddb22398f
Dan <60195449+ubu2020user@users.noreply.github.com>
2023-11-11 08:23:19 +0100

    Composable breath Excercise

--- app/src/main/java/com/junction/watchfrontend/presentation/composables/PersonDefaultStateEvaluationComposable.kt
+++ app/src/main/java/com/junction/watchfrontend/presentation/composables/PersonDefaultStateEvaluationComposable.kt
@@ -1,6 +1,5 @@
 package com.junction.watchfrontend.presentation.composables
 
-import android.content.Context
 import androidx.activity.ComponentActivity
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -10,7 +9,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
 import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -18,19 +16,16 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
-import com.junction.watchfrontend.R
 import com.junction.watchfrontend.presentation.theme.MyApplicationTheme
 
 @Composable
 fun personDefaultStateEvaluationComposable(activity: ComponentActivity) {
 
     var hasStarted by remember { mutableStateOf(false) }
-    var stage by remember { mutableStateOf(0) }
 
     MyApplicationTheme {
         Column(
@@ -51,27 +46,7 @@ fun personDefaultStateEvaluationComposable(activity: ComponentActivity) {
                 Button(
                     onClick = {
                         hasStarted = true
-                        android.os.Handler().postDelayed({
-                            stage = 0
-                            android.os.Handler().postDelayed({
-                                stage = 1
-                                android.os.Handler().postDelayed({
-                                    stage = 2
-                                    android.os.Handler().postDelayed({
-                                        stage = 0
-                                        android.os.Handler().postDelayed({
-                                            stage = 1
-                                            android.os.Handler().postDelayed({
-                                                stage = 2
-                                                android.os.Handler().postDelayed({
-                                                    stage = 3
-                                                }, 2000)
-                                            }, 2000)
-                                        }, 2000)
-                                    }, 2000)
-                                }, 2000)
-                            }, 2000)
-                        }, 2000)
+
                     },
                     modifier = Modifier
                         .minimumInteractiveComponentSize()
@@ -82,55 +57,7 @@ fun personDefaultStateEvaluationComposable(activity: ComponentActivity) {
 
 
             } else {
-                if (stage <= 0) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.outline_workspaces_24),
-                        "Breath in",
-                        tint = MaterialTheme.colors.primary,
-                    )
-                    Text(
-                        modifier = Modifier.fillMaxWidth(),
-                        textAlign = TextAlign.Center,
-                        text = "Breath in"
-                    )
-                } else if (stage == 1) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.baseline_more_horiz_24),
-                        "Breath in",
-                        tint = MaterialTheme.colors.primary,
-                    )
-                    Text(
-                        modifier = Modifier.fillMaxWidth(),
-                        textAlign = TextAlign.Center,
-                        text = "Hold"
-                    )
-                } else if (stage == 2) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.baseline_air_24),
-                        "Breath out",
-                        tint = MaterialTheme.colors.primary,
-                    )
-                    Text(
-                        modifier = Modifier.fillMaxWidth(),
-                        textAlign = TextAlign.Center,
-                        text = "Breath out"
-                    )
-                } else {
-
-                    Text(
-                        "Default Body State \nMeasurement\nfinished",
-                        textAlign = TextAlign.Center,
-                    )
-                    Spacer(modifier = Modifier.height(6.dp))
-                    Button(onClick = {
-                        activity.getSharedPreferences("prefs", Context.MODE_PRIVATE).edit()
-                            .putBoolean("isPersonsDefaultStateSaved", true).apply()
-                        // reload app
-                        activity.recreate()
-                    }) {
-                        Text("Finish")
-                    }
-                }
+                breathExcerciseComposable(activity, 1);
             }
         }
 
